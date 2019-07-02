1e451c89a8e44128fea01692e2220877763aaed1
Marcin Czenko <marcin@everydayproductive.com>
2019-07-02 13:14:57 +0200

    Corrected top level layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,44 +4,22 @@ import { Global } from '@emotion/core'
 import { DocumentationLayout } from './documentation'
 
 const Wrapper = styled.div({
-  display: 'flex',
-  flexFlow: 'column nowrap',
-  justifyContent: 'center',
-  width: '100vw',
-  backgroundColor: 'black'
+  padding: '25px'
 })
 
-const Layout = ({ location, children }) => {
-  if (location.pathname === '' || location.pathname === '/') {
-    return (
-      <Wrapper>
-        <Global styles={{
-          'html,body': {
-            backgroundColor: '#2F2E2D',
-            margin: 0,
-            padding: 0
-          }
-        }} />
-        {children}
-      </Wrapper>
-    )
-  } else {
-    return (
-      <div>
-        <Global styles={{
-          'html,body': {
-            margin: 0,
-            padding: 0,
-            boxSizing: 'border-box',
-            '@media (max-width: 768px)': {
-              fontSize: '112%'
-            }
-          }
-        }} />
-        <DocumentationLayout location={location}>{children}</DocumentationLayout>
-      </div>
-    )
-  }
-}
+const Layout = ({ location, children }) => (
+  <div>
+    <Global styles={{
+      'html,body': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box'
+      }
+    }} />
+    {(location.pathname === '' || location.pathname === '/')
+      ? <Wrapper>{ children }</Wrapper>
+      : <DocumentationLayout location={location}>{children}</DocumentationLayout>}
+  </div>
+)
 
 export default Layout
